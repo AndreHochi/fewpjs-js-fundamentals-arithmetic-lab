6f8195953af7a41de0a8849ac8ac995eb15f2ef4
Andrehochi <andrehochiwork@gmail.com>
2021-04-14 13:37:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+let num3 = 19
+let num4 = 15
+let array = [20]
+
+function multiply(num1, num2){
+    return(num1 * num2)
+}
+
+function random() {
+    return(Math.ceil(Math.random() * 10))
+}
+
+function mod(num3, num4) {
+    return(num3 % num4)
+}
+
+function max(array) {
+    Math.max(array)
+}
